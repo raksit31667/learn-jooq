cb404bd0ab3e818f1eca7b6084b4ead4e60c696d
raksit31667 <raksit.m@ku.th>
2021-11-01 20:34:16 +0700

    :tada:

---
+++ src/main/java/com/raksit/example/learnjooq/LearnJooqApplication.java
@@ -0,0 +1,13 @@
+package com.raksit.example.learnjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJooqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raksit/example/learnjooq/LearnJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raksit.example.learnjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
