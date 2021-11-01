ddb192936f302e8432a4409859cf0f805b0e1ecb
raksit31667 <raksit.m@ku.th>
2021-11-01 22:12:03 +0700

    :card_file_box: install Flyway and add simple database migration script

---
+++ src/main/resources/db/migration/V1.001__create_book_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE books
+(
+    id    SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(255)
+);
--- src/test/java/com/raksit/example/learnjooq/LearnJooqApplicationTests.java
+++ src/test/java/com/raksit/example/learnjooq/LearnJooqApplicationTests.java
@@ -3,10 +3,12 @@ package com.raksit.example.learnjooq;
 import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.DOCKER;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@FlywayTest
 @AutoConfigureEmbeddedDatabase(provider = DOCKER)
 class LearnJooqApplicationTests {
 
